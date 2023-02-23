39227e35cdb4411d537bc1e92e0a1e7a4d07b7ed
Suman Kisku <sumankisku1@gmail.com>
2023-02-24 02:06:18 +0530

    check

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const isbn = document.getElementById("isbn");
+const submit = document.getElementById("submit");
+const booklist = document.getElementById("book-list");
+
+submit.addEventListener("click", function(e) {
+    e.preventDefault();
+    booklist.innerHTML = `
+    <tr>
+    <td>${title.value}</td>
+    <td>${author.value}</td>
+    <td>${isbn.value}</td>
+    <td><i class="fa-solid fa-xmark delete"></i></td>
+    </tr>`
+});
